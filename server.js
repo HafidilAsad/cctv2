ae0500857811a0046e5c1b17515df05b7913fc41
Hafidil Asad <hafidilaksad@gmail.com>
2024-10-27 16:35:39 +0700

    add cors

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-Stream = require('node-rtsp-stream')
-stream = new Stream({
+const express = require('express');
+const cors = require('cors');
+const Stream = require('node-rtsp-stream');
+
+const app = express();
+
+// Gunakan middleware CORS
+app.use(cors()); // Mengizinkan semua sumber
+
+const stream = new Stream({
   name: 'name',
   streamUrl: 'rtsp://192.168.1.27:5543/051c6519288cc2d8b07f026902be8c96/live/channel0',
   wsPort: 9911,
-  ffmpegOptions: { // options ffmpeg flags
-    '-stats': '', // an option with no neccessary value uses a blank string
-    '-r': 30 // options with required values specify the value after the key
+  ffmpegOptions: {
+    '-stats': '',
+    '-r': 30
   }
-})
+});
+
+// Jalankan server (misalnya pada port 3000)
+app.listen(9912, () => {
+  console.log('Server running on port 3000');
+});
+
 
 
 // const onvif = require('node-onvif');
